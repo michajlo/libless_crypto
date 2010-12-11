13c0db31551c155d279d69257e4f843edb0d7671
michajlo <michajlo.matijkiw@gmail.com>
2010-12-11 16:16:22 -0500

    Wrote (but not tested) mix columns, sub bytes, shift rows, and add key.  Figuring out how to input and output, make it elegant.

--- aes/aes.h
+++ aes/aes.h
@@ -11,14 +11,19 @@ typedef struct {
     uint32_t key_size;
     uint32_t expanded_key_len;
     uint32_t num_rounds;
+    uint32_t ;
+    uint8_t state[16];
     uint8_t *expanded_key;
 } aes_t;
 
+#define AES_CIPHERTEXT_SIZE(l) ((((l + 15)/16) * 16)
 
 int aes_init(aes_t *aes, uint32_t aes_type, uint8_t *key, uint8_t key_len);
 int aes_destroy(aes_t *aes);
 
-int aes_encrypt(aes_t *aes, uint8_t *in, uint32_t in_len, uint8_t *out, uint32_t outlen);
-int aes_decrypt(aes_t *aes, uint8_t *in, uint32_t in_len, uint8_t *out, uint32_t outlen);
+int aes_encrypt_block(aes_t *aes, uint8_t *in, uint8_t *out);
+int aes_decrypt_block(aes_t *aes, uint8_t *in, uint8_t *out);
 
+int aes_encrypt(aes_t *aes, uint8_t *in, uint32_t in_len, uint8_t *out);
+int aes_decrypt(aes_t *aes, uint8_t *in, uint32_t in_len, uint8_t *out);
 #endif
